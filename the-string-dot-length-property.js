89bc591f3cd06b922ec89d6ad538fcb0a2b0747b
Melissa <meliss.williams@gmail.com>
2025-10-19 18:25:33 -0400

    initial commit

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -25,6 +25,11 @@ let lengthTwo;   // Length of stringTwo
 let lengthThree; // Length of stringThree
 
 // Your code here
-
+lengthOne = stringOne.length;
+lengthTwo = stringTwo.length;
+lengthThree = stringThree.length;
+console.log(lengthOne);
+console.log(lengthTwo);
+console.log(lengthThree);
 
 
